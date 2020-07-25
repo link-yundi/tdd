c4e27b66a5496ecd18af9d62523fcee3e8b43e62
link-yundi <link.yundi@foxmail.com>
2020-07-25 11:36:39 +0800

    deleted old ch06 & add new ch06 demo

---
+++ ch06_pointer_error/wallet_test.go
@@ -0,0 +1,45 @@
+package ch06_pointer_error
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type Bitcoin int
+
+// 定义Bitcoin的String()
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	// 定义私有变量
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func TestDepositWithdraw(t *testing.T) {
+	wallet := Wallet{Bitcoin(10)}
+	assert := assert.New(t)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet.Deposit(Bitcoin(10))
+		t.Logf("%s", wallet.balance)
+		assert.Equal(Bitcoin(20), wallet.Balance())
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet.Withdraw(Bitcoin(10))
+		assert.Equal(Bitcoin(10), wallet.Balance())
+	})
+}
